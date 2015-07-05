6cbe1f5e0bf1f7b432921265f9b7286ac7781c45
Tomáš Vyčítal <tom.vycital@gmail.com>
2015-07-05 16:48:05 +0200

    Count updateCountDown segment coordinates from map size (3/4 of map width/height)

--- www/js/updateCountDown.js
+++ www/js/updateCountDown.js
@@ -1,6 +1,8 @@
 function updateCountDown() {
     if ( game.vars.state === "run" && ++game.vars.update >= 60 ) {
-        var segment = game.vars.segments[ game.vars.segments.length - 2 ][ 1 ];
+        var col     = Math.floor( game.vars.segments.length * 3 / 4 ),
+            row     = Math.floor( game.vars.segments[ col ].length * 3 / 4 ),
+            segment = game.vars.segments[ col ][ row ];
         if ( game.vars.countDown > 0 ) {
             game.vars.update = 0;
             segment.setState( SevenSegment.prototype.states[ game.vars.countDown-- ] );
