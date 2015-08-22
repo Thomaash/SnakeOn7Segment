22a8f1206b8d9104f6d8985baea47e2a746cc86a
Tomáš Vyčítal <tom.vycital@gmail.com>
2015-08-22 20:36:49 +0200

    Save level in localStorage

--- www/js/Level.js
+++ www/js/Level.js
@@ -1,16 +1,19 @@
-function Level( value, precision, min, max ) {
+function Level( defaultValue, precision, min, max ) {
     this.precision = Math.pow( 10, precision );
-    this.level = value * this.precision;
+    this.default = defaultValue * this.precision;
     this.min = min * this.precision;
     this.max = max * this.precision;
 
     this.add = function ( int, decimal ) {
-        this.level += int * this.precision + decimal;
-        this.limit();
+        this.change( int, decimal );
     };
     this.sub = function ( int, decimal ) {
-        this.level -= int * this.precision + decimal;
+        this.change( -int, -decimal );
+    };
+    this.change = function ( int, decimal ) {
+        this.level += int * this.precision + decimal;
         this.limit();
+        this.save();
     };
     this.floor = function () {
         return Math.floor( this.level / this.precision );
@@ -22,4 +25,21 @@ function Level( value, precision, min, max ) {
             this.level = this.max;
         }
     };
+    this.load = function () {
+        var item = window.localStorage.getItem( "So7S_level" );
+        if ( item == null ) {
+            this.level = this.default;
+        } else {
+            this.level = item * 1;
+            this.limit();
+        }
+
+        console.log( this.level );
+    };
+    this.save = function () {
+        window.localStorage.setItem( "So7S_level", this.level );
+    };
+
+    // Load saved value or use default
+    this.load();
 }
