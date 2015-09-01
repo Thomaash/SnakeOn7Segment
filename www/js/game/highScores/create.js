5e5e39ce20fb7fae82c6c4e7263f0a7e9d5bb229
Tomáš Vyčítal <tom.vycital@gmail.com>
2015-09-01 18:06:19 +0200

    Add some text into high score labels

--- www/js/game/highScores/create.js
+++ www/js/game/highScores/create.js
@@ -3,13 +3,27 @@ define( [ "ui/createButton", "tools/score" ], function ( createButton, score ) {
         game.state.start( "MainMenu" );
     }
 
+    function ordinal( number ) {
+        switch ( String( number ).slice( -1 ) ) {
+            case "1":
+                return number + "st";
+            case "2":
+                return number + "nd";
+            case "3":
+                return number + "rd";
+            default:
+                return number + "th";
+        }
+    }
+
     return function () {
         createButton( { x: 200, y: 40 }, "Menu", clickMenu );
 
         var scores = score.load();
         if ( scores != null ) {
+            var offset = Math.floor( scores.length / 2 ) + 1;
             for ( var i = 0; i < scores.length && i < 9; i++ ) {
-                createButton( i - 5, (i + 1) + ": " + scores[ i ] );
+                createButton( i - offset, ordinal( i + 1 ) + ": " + scores[ i ] + " LEDs" );
             }
         } else {
             createButton( 0, "No scores yet", clickMenu );
