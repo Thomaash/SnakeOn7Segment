d1f4eb7c68b200307e50238ab40eeb0e018e7562
Tomáš Vyčítal <tom.vycital@gmail.com>
2015-09-06 19:44:47 +0200

    Use method for changing game size instead of altering attributes directly

--- www/js/tools/changeSize.js
+++ www/js/tools/changeSize.js
@@ -3,10 +3,9 @@ define( [], function () {
         width = typeof width === "number" ? width : 1200;
         height = typeof height === "number" ? height : 720;
 
-        game.width = width;
-        game.height = height;
-        game.world.width = width;
-        game.world.height = height;
+        game.world.width = width; // Resize world
+        game.world.height = height; // Resize world
+        game.scale.setGameSize( width, height ); // Tell Phaser to resize canvas
         game.renderer.resize( width, height ); // Tell renderer to resize canvas
         game.scale.refresh(); // Tell scale manager to scale to new canvas size
     };
