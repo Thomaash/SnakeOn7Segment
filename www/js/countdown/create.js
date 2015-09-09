2770495d75503a26925ff377249116381711074d
Tomáš Vyčítal <tom.vycital@gmail.com>
2015-09-09 18:07:08 +0200

    Add lines to countdown screen to makes control help clearer

--- www/js/countdown/create.js
+++ www/js/countdown/create.js
@@ -15,12 +15,14 @@ define(
             game.vars.countDown = 3;
             game.vars.update = 60;
 
+            // Groups
+            var leds  = game.add.group(),
+                lines = game.add.group(),
+                ui    = game.add.group();
+
             // Create menu button
             button( 200, 40, "Menu", clickMenu, "button" );
 
-            // Groups
-            var leds = game.add.group(),
-                ui   = game.add.group();
 
             // Help UI arrows
             if ( game.vars.multiplayer ) {
@@ -33,8 +35,28 @@ define(
                 createArrow( ui, game.world.width * 0.8, game.world.height * 0.5, 180 );
             }
 
+
+            // Help UI lines
+            var graphics = game.add.graphics( 0, 0 );
+            graphics.lineStyle( 4, 0x444444 );
+
+            // Vertical line
+            graphics.moveTo( game.world.width / 2, 0 );
+            graphics.lineTo( game.world.width / 2, game.world.height );
+
+            // Horizontal line
+            if ( game.vars.multiplayer ) {
+                graphics.moveTo( 0, game.world.height / 2 );
+                graphics.lineTo( game.world.width, game.world.height / 2 );
+            }
+
+            // Add lines to group
+            lines.add( graphics );
+
+
             // Help UI text
             centerScreenText( "Tap left half of the screen to turn left, right to turn right.", ui );
+
 
             // Create segment
             game.vars.segments = new SevenSegment( leds, 3, 1 );
