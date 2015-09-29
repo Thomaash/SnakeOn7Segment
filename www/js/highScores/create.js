67ff57e80f9f1ebf2791adedfd9170f96e578af7
Tomáš Vyčítal <tom.vycital@gmail.com>
2015-09-29 19:46:19 +0200

    Remove duplicate code from highScores, change LEDs to points in highScores

--- www/js/highScores/create.js
+++ www/js/highScores/create.js
@@ -1,54 +1,42 @@
 define(
     [ "ui/button", "tools/score", "tools/ordinal" ],
     function ( button, score, ordinal ) {
-        function clickMenu() {
-            game.state.start( "MainMenu" );
-        }
+        var click = {
+            menu : function () {
+                game.state.start( "MainMenu" );
+            },
+            reset: function () {
+                score.reset();
+                game.state.start( "HighScores" );
+            }
+        };
 
-        function clickReset() {
-            score.reset();
-            game.state.start( "HighScores" );
+        function showScores( title, scores, x, y ) {
+            // Header
+            button( x, y, title, null, "button" );
+            y += 80;
+
+            // Scores
+            if ( scores != null ) {
+                for ( var i = 0; i < scores.length && i < 9; i++ ) {
+                    button( x, y, ordinal( i + 1 ) + ": " + scores[ i ] + " points", null, "button" );
+                    y += 80;
+                }
+            } else {
+                button( x, y, "No scores yet", null, "button" );
+            }
         }
 
         return function () {
-            var x, y, i,
-                scores = score.load();
+            var scores = score.load();
 
             // Menu and reset buttons
-            button( 200, 72, "Menu", clickMenu, "button" );
-            button( 200, game.world.height - 72, "Reset", clickReset, "button" );
+            button( 200, 72, "Menu", click.menu, "button" );
+            button( 200, game.world.height - 72, "Reset", click.reset, "button" );
 
-            // Classic
-            x = game.world.centerX;
-            y = 40;
-            // Header
-            button( x, y, "Classic", null, "button" );
-            y += 80;
-            // Scores
-            if ( scores.classic != null ) {
-                for ( i = 0; i < scores.classic.length && i < 9; i++ ) {
-                    button( x, y, ordinal( i + 1 ) + ": " + scores.classic[ i ] + " LEDs", null, "button" );
-                    y += 80;
-                }
-            } else {
-                button( x, y, "No scores yet", null, "button" );
-            }
-
-            // Single
-            x = game.world.centerX + 360;
-            y = 40;
-            // Header
-            button( x, y, "Single", null, "button" );
-            y += 80;
-            // Scores
-            if ( scores.single != null ) {
-                for ( i = 0; i < scores.single.length && i < 9; i++ ) {
-                    button( x, y, ordinal( i + 1 ) + ": " + scores.single[ i ] + " LEDs", null, "button" );
-                    y += 80;
-                }
-            } else {
-                button( x, y, "No scores yet", null, "button" );
-            }
+            // High scores
+            showScores( "Classic", scores.classic, game.world.centerX, 40 );
+            showScores( "Single", scores.single, game.world.centerX + 360, 40 );
         };
     }
 );
